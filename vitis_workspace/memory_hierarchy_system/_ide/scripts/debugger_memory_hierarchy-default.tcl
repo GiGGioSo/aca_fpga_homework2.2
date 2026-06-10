5c5ca9a375a4653aa2bdb9b69b700da94c93172d
GiGGioSo <gioele.pettarini@gmail.com>
2026-06-10 17:46:28 +0200

    first commit

---
+++ vitis_workspace/memory_hierarchy_system/_ide/scripts/debugger_memory_hierarchy-default.tcl
@@ -0,0 +1,30 @@
+# Usage with Vitis IDE:
+# In Vitis IDE create a Single Application Debug launch configuration,
+# change the debug type to 'Attach to running target' and provide this 
+# tcl script in 'Execute Script' option.
+# Path of this script: /home/gio/uni/aca/fpga/aca_fpga_homework2.2/vitis_workspace/memory_hierarchy_system/_ide/scripts/debugger_memory_hierarchy-default.tcl
+# 
+# 
+# Usage with xsct:
+# To debug using xsct, launch xsct and run below command
+# source /home/gio/uni/aca/fpga/aca_fpga_homework2.2/vitis_workspace/memory_hierarchy_system/_ide/scripts/debugger_memory_hierarchy-default.tcl
+# 
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zybo Z7 210351BA1DB5A" && level==0 && jtag_device_ctx=="jsn-Zybo Z7-210351BA1DB5A-23727093-0"}
+fpga -file /home/gio/uni/aca/fpga/aca_fpga_homework2.2/vitis_workspace/memory_hierarchy/_ide/bitstream/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw /home/gio/uni/aca/fpga/aca_fpga_homework2.2/vitis_workspace/design_1_wrapper/export/design_1_wrapper/hw/design_1_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source /home/gio/uni/aca/fpga/aca_fpga_homework2.2/vitis_workspace/memory_hierarchy/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "*microblaze*#0" && bscan=="USER2" }
+dow /home/gio/uni/aca/fpga/aca_fpga_homework2.2/vitis_workspace/memory_hierarchy/Debug/memory_hierarchy.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*microblaze*#0" && bscan=="USER2" }
+con
